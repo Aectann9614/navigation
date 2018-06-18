Fixed quaternions for numpy vector

bins/quat.py
@@ -4,25 +4,20 @@ from math import sqrt
 
 def is_quat(that):
     if isinstance(that, np.ndarray):
-        if that.shape == (3, 1) and that.dtype == float:
+        if that.shape == (4,) and that.dtype == float:
             return True
     return False
 
 
-def is_quat_decorator(func):
-    def call(*args, **kwargs):
-        return func(*args, **kwargs)
-    return call
-
-
-def quat(q0, q1, q2, q3):
-    if isinstance(q0, float) and isinstance(q0, float) and isinstance(q0, float) and isinstance(q0, float):
-        return np.array([[q0], [q1], [q2], [q3]], float)
+def quat(quat_params):
+    if len(quat_params) == 4 and isinstance(quat_params[0], float) and isinstance(quat_params[1], float) \
+            and isinstance(quat_params[2], float) and isinstance(quat_params[3], float):
+        return np.array([quat_params[0], quat_params[1], quat_params[2], quat_params[3]], float)
     raise TypeError("Params of quaternion must be float")
 
 
 def zero_quat():
-    return quat(0, 0, 0, 0)
+    return quat((0.0, 0.0, 0.0, 0.0))
 
 
 def extend_matrix(that):
@@ -66,4 +61,4 @@ def normalize(that, eps=0.001):
 
 
 def multiply(that, other):
-    return np.dot(extend_matrix(that), other)
+    return np.dot(extend_matrix(that), other.copy().resize(4, 1))